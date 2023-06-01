f9ae4ea5b08cc3ffe38aa1d0e4ffa816ca7aba72
tung trinh <tungtrinh99@gmail.com>
2023-06-02 00:39:48 +0700

    fix

---
+++ prisma/seed.js
@@ -0,0 +1,38 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function main() {
+  const tungTrinh = await prisma.user.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      firstName: 'Trinh Tung',
+      lastName: 'Tung',
+      avatar: 'https://i.imgur.com/EJKHmUw.png',
+    },
+  })
+
+  const yenNhi = await prisma.woman.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      firstName: 'Truong Yen',
+      lastName: 'Nhi',
+      username: 'nhi.truong',
+      avatar: 'https://i.imgur.com/EJKHmUw.png',
+    },
+  })
+  
+  console.log({ tungTrinh, yenNhi })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
