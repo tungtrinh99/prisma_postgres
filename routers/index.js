26f9186e74303d524361e373a0880f894ad447cb
tung trinh <tungtrinh99@gmail.com>
2023-06-09 09:59:37 +0700

    fixbug

--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,10 @@
 const router = require('./api');
 
 exports.routers = (app) => {
+    app.use('/api/v1', router);
     app.get('/', (req, res) => {
         res.json({
             message: 'Hello World'
         })
     });
-    app.use('/api/v1', router)
 }
